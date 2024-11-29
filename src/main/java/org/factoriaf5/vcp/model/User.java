41bd43c36a5ff0b19f359daf9425aa19f8ff1268
OlenaMyroshnykova <elenamirespania@gmail.com>
2024-11-29 14:25:31 +0100

    git commit -m "VCP-25 added spring security, user, user service, user repository, user controller"

--- src/main/java/org/factoriaf5/vcp/model/User.java
+++ src/main/java/org/factoriaf5/vcp/model/User.java
@@ -1,5 +1,76 @@
 package org.factoriaf5.vcp.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class User {
 
+    private static long idCounter = 0;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private String phone;
+
+    public User() {}
+
+    public User(String username, String password, Role role, String phone) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.phone = phone;
+        this.id = generateId();
+    }
+
+    private static synchronized long generateId() {
+        return ++idCounter;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public enum Role {
+        ADMIN,
+        USER
+    }
 }
