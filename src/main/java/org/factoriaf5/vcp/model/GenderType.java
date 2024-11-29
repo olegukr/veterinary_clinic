ff8ba2df1a80cd334d358cb223a968bb8afeaa07
OMuzalevska <oksana.muzalevskaja@gmail.com>
2024-11-29 10:31:35 +0100

    VCP-24: add class Patient & enum GenderType

---
+++ src/main/java/org/factoriaf5/vcp/model/GenderType.java
@@ -0,0 +1,7 @@
+package org.factoriaf5.vcp.model;
+
+public enum GenderType {
+    M, 
+    W, 
+    NB
+}
--- src/main/java/org/factoriaf5/vcp/model/Patient.java
+++ src/main/java/org/factoriaf5/vcp/model/Patient.java
@@ -1,5 +1,128 @@
 package org.factoriaf5.vcp.model;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class Patient {
+    
+    private static int idCounter = 1;
+
+    private final long id;
+    private String name;
+    private Long idUser;
+    private int age;
+    private String breed;
+    private GenderType gender;
+    private String imageUrl;
+
+   
+public Patient(Long idPatient, String name, Long idUser, int age, String breed, GenderType gender, String imageUrl) {
+    //Long idUser - ?
+    this.id = idCounter++;
+    this.name = name;
+    this.idUser = idUser;
+    this.age = age;
+    this.breed = breed;
+    this.gender = gender;
+    this.imageUrl = imageUrl;
+}
+
+
+public Long getIdUser() {
+    return idUser;
+}
+
+
+public static int getIdCounter() {
+    return idCounter;
+}
+
+
+public long getId() {
+    return id;
+}
+
+
+public String getName() {
+    return name;
+}
+
+
+public int getAge() {
+    return age;
+}
+
+
+public String getBreed() {
+    return breed;
+}
+
+
+public GenderType getGender() {
+    return gender;
+}
+
+
+public String getImageUrl() {
+    return imageUrl;
+}
+
+
+public static void setIdCounter(int idCounter) {
+    Patient.idCounter = idCounter;
+}
+
+
+public void setIdUser(Long idUser) {
+    this.idUser = idUser;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+
+public void setAge(int age) {
+    this.age = age;
+}
+
+
+public void setBreed(String breed) {
+    this.breed = breed;
+}
+
+
+public void setGender(GenderType gender) {
+    this.gender = gender;
+}
+
+
+public void setImageUrl(String imageUrl) {
+    this.imageUrl = imageUrl;
+}
+
+// Методи
+    public void addPatient(Patient patient) {
+        // ...
+    }
+
+    // public Patient getPatientById(Long id) {
+    //     // ...
+    //     return new Patient(); 
+    //     }
+
+    public void updatePatient(Patient patient) {
+        // ...
+    }
+
+    public void deletePatient(Long id) {
+        // ...
+    }
+
+    public String uploadPhoto(Long patientId, MultipartFile photo) {
+        // завантаження фото
+        return "URL photo"; 
+    }
+
+
 
 }
