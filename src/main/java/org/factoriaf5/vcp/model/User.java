8fa16d227d0e5d025af2fd232cadf14d1c0f8fb0
olegukr <oleg.poberezhets@gmail.com>
2024-12-03 21:05:48 +0100

    add data.sql and @Entity @Table(name='users'

--- src/main/java/org/factoriaf5/vcp/model/User.java
+++ src/main/java/org/factoriaf5/vcp/model/User.java
@@ -1,8 +1,15 @@
 package org.factoriaf5.vcp.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     private static long idCounter = 0;
