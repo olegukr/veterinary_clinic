17f2d3be93816316a7512ef2a5f65bf3dffa4068
OlenaMyroshnykova <elenamirespania@gmail.com>
2024-12-10 14:31:42 +0100

    added UserDTO

---
+++ src/main/java/org/factoriaf5/dto/UserDTO.java
@@ -0,0 +1,41 @@
+package org.factoriaf5.dto;
+
+public class UserDTO {
+    private String username;
+    private String password;
+    private String usertype;
+    private String phone;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsertype() {
+        return usertype;
+    }
+
+    public void setUsertype(String usertype) {
+        this.usertype = usertype;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
+
